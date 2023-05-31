0ad4fd8abd3ce6e3a14fbeb8cb2b18e0ca4a85e0
Makerfabs <>
2023-05-31 18:47:11 +0800

    update demo

---
+++ ESP32S3_4.0/Basic usage code in Squareline V1.3.0/SquareLine_Project/ui/ui.ino
@@ -0,0 +1,127 @@
+#include <lvgl.h>
+#include <Arduino_GFX_Library.h>
+#include <ui.h>
+
+/*Don't forget to set Sketchbook location in File/Preferencesto the path of your UI project (the parent foder of this INO file)*/
+
+/*Change to your screen resolution*/
+static const uint16_t screenWidth  = 480;
+static const uint16_t screenHeight = 480;
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[ screenWidth * screenHeight / 10 ];
+
+Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
+    1 /* CS */, 12 /* SCK */, 11 /* SDA */,
+    45 /* DE */, 4 /* VSYNC */, 5 /* HSYNC */, 21 /* PCLK */,
+    39 /* R0 */, 40 /* R1 */, 41 /* R2 */, 42 /* R3 */, 2 /* R4 */,
+    0 /* G0/P22 */, 9 /* G1/P23 */, 14 /* G2/P24 */, 47 /* G3/P25 */, 48 /* G4/P26 */, 3 /* G5 */,
+    6 /* B0 */, 7 /* B1 */, 15 /* B2 */, 16 /* B3 */, 8 /* B4 */
+);
+
+Arduino_ST7701_RGBPanel *gfx = new Arduino_ST7701_RGBPanel(
+    bus, GFX_NOT_DEFINED /* RST */, 0 /* rotation */,
+    true /* IPS */, 480 /* width */, 480 /* height */,
+    st7701_type1_init_operations, sizeof(st7701_type1_init_operations),
+    true /* BGR */);
+
+#include "touch.h"
+
+#if LV_USE_LOG != 0
+/* Serial debugging */
+void my_print(const char * buf)
+{
+    Serial.printf(buf);
+    Serial.flush();
+}
+#endif
+
+/* Display flushing */
+void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
+{
+    uint32_t w = ( area->x2 - area->x1 + 1 );
+    uint32_t h = ( area->y2 - area->y1 + 1 );
+
+#if (LV_COLOR_16_SWAP != 0)
+    gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#else
+    gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#endif
+
+    lv_disp_flush_ready( disp );
+}
+
+/*Read the touchpad*/
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+    if (touch_has_signal())
+    {
+        if (touch_touched())
+        {
+            data->state = LV_INDEV_STATE_PR;
+
+            /*Set the coordinates*/
+            data->point.x = touch_last_x;
+            data->point.y = touch_last_y;
+        }
+        else if (touch_released())
+        {
+            data->state = LV_INDEV_STATE_REL;
+        }
+    }
+    else
+    {
+        data->state = LV_INDEV_STATE_REL;
+    }
+}
+
+void setup()
+{
+    Serial.begin( 115200 ); /* prepare for possible serial debug */
+
+    String LVGL_Arduino = "Hello Arduino! ";
+    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+
+    Serial.println( LVGL_Arduino );
+    Serial.println( "I am LVGL_Arduino" );
+
+    touch_init();
+    gfx->begin();
+    gfx->fillScreen(BLACK);
+
+    lv_init();
+
+#if LV_USE_LOG != 0
+    lv_log_register_print_cb( my_print ); /* register print function for debugging */
+#endif
+
+    lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * screenHeight / 10 );
+
+    /*Initialize the display*/
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init( &disp_drv );
+    /*Change the following line to your display resolution*/
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register( &disp_drv );
+
+    /*Initialize the (dummy) input device driver*/
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init( &indev_drv );
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register( &indev_drv );
+
+
+    ui_init();
+
+    Serial.println( "Setup done" );
+}
+
+void loop()
+{
+    lv_timer_handler(); /* let the GUI do its work */
+    delay(5);
+}
